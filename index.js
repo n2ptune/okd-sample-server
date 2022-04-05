f522f04213c32127c000fb6b9925ad345782b9ec
n2ptune <n2ptune@outlook.com>
2022-04-05 17:33:35 +0900

    test

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mysql = require('mysql2/promise')
+
+function getConnection () {
+	return mysql.createConnection({
+		host: process.env.DB_HOST,
+		database: 'khlee_test',
+		port: 3306,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD
+	})
+}
 
 const devNotes = []
 let id = 0
@@ -8,17 +19,26 @@ let id = 0
 app.use(cors())
 app.use(express.json())
 
-app.get('/notes', (req, res) => {
-	res.json(devNotes).end()
+app.get('/notes', async (req, res) => {
+	const conn = await getConnection()
+	const q = await conn.query('SELECT * FROM khlee_test')
+	res.json(q).end()
 })
 
 app.get('/note/:id', (req, res) => {
-	res.json(devNotes.find(note => note.id === req.params.id)).end()
+	// res.json(devNotes.find(note => note.id === req.params.id)).end()
+	const conn = await getConnection()
+	const q = await conn.query('SELECT * FROM khlee_test WHERE id = ?', req.params.id)
+	res.json(q).end()
 })
 
 app.post('/note', (req, res) => {
-	devNotes.push({ value: req.body.value, id: id++, createDate: new Date() })
-	res.status(200).end()
+	// devNotes.push({ value: req.body.value, id: id++, createDate: new Date() })
+	// res.status(200).end()
+	const conn = await getConnection()
+	const rand = Math.floor(Math.random() * 100000000)
+	const q = await conn.query('INSERT INTO khlee_test (id, value) VALUES (?, ?)', [rand, req.body.value || ''])
+	res.json(q).end()
 })
 
 app.listen(3033, () => console.log('server start'))
